1c7b74de955959c2e9bfd60e28dd8a8efb539edd
Horacio Leon <horacio.leon@arkusnexus.com>
2023-08-01 13:26:19 -0600

    feat: completed dashbaord and project enhacenment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { loginSuccess } from "./features/login/loginSlice"
 import { store } from "./app/store"
 import Login from "./features/login/Login"
+import Dashboard from "./features/dashboard/Dashboard"
 import "./App.css"
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<div>Dashboard</div>} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
